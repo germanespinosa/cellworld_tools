73c053474e5982d684499324f94c142b64c72eda
German Espinosa <germanespinosa@gmail.com>
2021-10-28 15:54:17 -0500

    Delete cmake-build-debug directory

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Research/robot/cellworld_tools
-# Build directory: /mnt/c/Research/robot/cellworld_tools/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(cellworld_toolsTests ".tests/cellworld_tools_tests")
-set_tests_properties(cellworld_toolsTests PROPERTIES  _BACKTRACE_TRIPLES "/usr/local/share/CatchTests/catchtests.cmake;19;add_test;/usr/local/share/CatchTests/catchtests.cmake;0;;/usr/local/share/cmake/CatchTests/CatchTestsConfig.cmake;91;include;/usr/local/share/cmake/CatchTests/CatchTestsConfig.cmake;0;;/mnt/c/Research/robot/cellworld_tools/CMakeLists.txt;78;find_package;/mnt/c/Research/robot/cellworld_tools/CMakeLists.txt;0;")
